cf17cc1cba30114e4c9c7110f48e4fa24cdf05fc
Rob Walworth <robert.walworth@swirldslabs.com>
2024-12-13 20:06:32 -0500

    refactor: style and mark tests implemented
    Signed-off-by: Rob Walworth <robert.walworth@swirldslabs.com>

--- src/tests/token-service/test-token-revoke-kyc-transaction.ts
+++ src/tests/token-service/test-token-revoke-kyc-transaction.ts
@@ -28,49 +28,56 @@ describe("TokenRevokeKycTransaction", function () {
       process.env.OPERATOR_ACCOUNT_PRIVATE_KEY as string,
     );
 
-    let response = await JSONRPCRequest(this, "generateKey", {
-      type: "ed25519PrivateKey",
-    });
-    tokenFreezeKey = response.key;
+    tokenFreezeKey = (
+      await JSONRPCRequest(this, "generateKey", {
+        type: "ed25519PrivateKey",
+      })
+    ).key;
 
-    response = await JSONRPCRequest(this, "generateKey", {
-      type: "ed25519PrivateKey",
-    });
-    tokenAdminKey = response.key;
+    tokenAdminKey = (
+      await JSONRPCRequest(this, "generateKey", {
+        type: "ed25519PrivateKey",
+      })
+    ).key;
 
-    response = await JSONRPCRequest(this, "generateKey", {
-      type: "ecdsaSecp256k1PrivateKey",
-    });
-    tokenPauseKey = response.key;
+    tokenPauseKey = (
+      await JSONRPCRequest(this, "generateKey", {
+        type: "ecdsaSecp256k1PrivateKey",
+      })
+    ).key;
 
-    response = await JSONRPCRequest(this, "generateKey", {
-      type: "ecdsaSecp256k1PrivateKey",
-    });
-    tokenKycKey = response.key;
+    tokenKycKey = (
+      await JSONRPCRequest(this, "generateKey", {
+        type: "ecdsaSecp256k1PrivateKey",
+      })
+    ).key;
 
-    response = await JSONRPCRequest(this, "createToken", {
-      name: "testname",
-      symbol: "testsymbol",
-      treasuryAccountId: process.env.OPERATOR_ACCOUNT_ID,
-      adminKey: tokenAdminKey,
-      kycKey: tokenKycKey,
-      freezeKey: tokenFreezeKey,
-      pauseKey: tokenPauseKey,
-      commonTransactionParams: {
-        signers: [tokenAdminKey],
-      },
-    });
-    tokenId = response.tokenId;
+    tokenId = (
+      await JSONRPCRequest(this, "createToken", {
+        name: "testname",
+        symbol: "testsymbol",
+        treasuryAccountId: process.env.OPERATOR_ACCOUNT_ID,
+        adminKey: tokenAdminKey,
+        kycKey: tokenKycKey,
+        freezeKey: tokenFreezeKey,
+        pauseKey: tokenPauseKey,
+        commonTransactionParams: {
+          signers: [tokenAdminKey],
+        },
+      })
+    ).tokenId;
 
-    response = await JSONRPCRequest(this, "generateKey", {
-      type: "ed25519PrivateKey",
-    });
-    accountPrivateKey = response.key;
+    accountPrivateKey = (
+      await JSONRPCRequest(this, "generateKey", {
+        type: "ed25519PrivateKey",
+      })
+    ).key;
 
-    response = await JSONRPCRequest(this, "createAccount", {
-      key: accountPrivateKey,
-    });
-    accountId = response.accountId;
+    accountId = (
+      await JSONRPCRequest(this, "createAccount", {
+        key: accountPrivateKey,
+      })
+    ).accountId;
 
     await JSONRPCRequest(this, "associateToken", {
       accountId,
@@ -121,7 +128,9 @@ describe("TokenRevokeKycTransaction", function () {
         },
       });
 
-      await retryOnError(async () => verifyTokenNoKyc(accountId, tokenId));
+      await retryOnError(async function () {
+        await verifyTokenNoKyc(accountId, tokenId);
+      });
     });
 
     it("(#2) Revokes KYC of a token that doesn't exist to an account", async function () {
@@ -221,17 +230,18 @@ describe("TokenRevokeKycTransaction", function () {
     });
 
     it("(#8) Revokes KYC of a token to an account but signs with an incorrect private key", async function () {
-      const response = await JSONRPCRequest(this, "generateKey", {
-        type: "ed25519PrivateKey",
-      });
-      const key = response.key;
-
       try {
         await JSONRPCRequest(this, "revokeTokenKyc", {
           tokenId,
           accountId,
           commonTransactionParams: {
-            signers: [key],
+            signers: [
+              (
+                await JSONRPCRequest(this, "generateKey", {
+                  type: "ed25519PrivateKey",
+                })
+              ).key,
+            ],
           },
         });
       } catch (err: any) {
@@ -243,16 +253,15 @@ describe("TokenRevokeKycTransaction", function () {
     });
 
     it("(#9) Revokes KYC of a token with no KYC key to an account", async function () {
-      const response = await JSONRPCRequest(this, "createToken", {
-        name: "testname",
-        symbol: "testsymbol",
-        treasuryAccountId: process.env.OPERATOR_ACCOUNT_ID,
-      });
-      const tokenIdNoKyc = response.tokenId;
-
       try {
         await JSONRPCRequest(this, "revokeTokenKyc", {
-          tokenId: tokenIdNoKyc,
+          tokenId: (
+            await JSONRPCRequest(this, "createToken", {
+              name: "testname",
+              symbol: "testsymbol",
+              treasuryAccountId: process.env.OPERATOR_ACCOUNT_ID,
+            })
+          ).tokenId,
           accountId,
         });
       } catch (err: any) {
@@ -280,7 +289,9 @@ describe("TokenRevokeKycTransaction", function () {
         },
       });
 
-      await retryOnError(async () => verifyTokenNoKyc(accountId, tokenId));
+      await retryOnError(async function () {
+        await verifyTokenNoKyc(accountId, tokenId);
+      });
     });
 
     it("(#11) Revokes KYC of a token to an account that is not associated with the token", async function () {
