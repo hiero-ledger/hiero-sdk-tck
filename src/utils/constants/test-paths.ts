2a1dad69dee1eb78b5febc81c89f320cb391cf28
Rob Walworth <110835868+rwalworth@users.noreply.github.com>
2025-04-02 09:35:41 -0400

    fix: flaky tests (#370)
    Signed-off-by: Rob Walworth <robert.walworth@swirldslabs.com>

--- src/utils/constants/test-paths.ts
+++ src/utils/constants/test-paths.ts
@@ -1,29 +1,31 @@
 export const TEST_CONFIGURATIONS: Record<string, string> = {
   // Crypto Service Tests
-  AccountCreate: "src/tests/crypto-service/test-account-create-transaction.ts",
-  AccountUpdate: "src/tests/crypto-service/test-account-update-transaction.ts",
-  AccountDelete: "src/tests/crypto-service/test-account-delete-transaction.ts",
-  AccountAllowanceDelete:
-    "src/tests/crypto-service/test-account-allowance-delete-transaction.ts",
   AccountAllowanceApprove:
     "src/tests/crypto-service/test-account-allowance-approve-transaction.ts",
+  AccountAllowanceDelete:
+    "src/tests/crypto-service/test-account-allowance-delete-transaction.ts",
+  AccountCreate: "src/tests/crypto-service/test-account-create-transaction.ts",
+  AccountDelete: "src/tests/crypto-service/test-account-delete-transaction.ts",
+  AccountUpdate: "src/tests/crypto-service/test-account-update-transaction.ts",
+  TransferCrypto: "src/tests/crypto-service/test-transfer-transaction.ts",
   // Token Service Tests
-  TokenCreate: "src/tests/token-service/test-token-create-transaction.ts",
-  TokenUpdate: "src/tests/token-service/test-token-update-transaction.ts",
-  TokenDelete: "src/tests/token-service/test-token-delete-transaction.ts",
-  TokenBurn: "src/tests/token-service/test-token-burn-transaction.ts",
-  TokenMint: "src/tests/token-service/test-token-mint-transaction.ts",
   TokenAssociate: "src/tests/token-service/test-token-associate-transaction.ts",
+  TokenBurn: "src/tests/token-service/test-token-burn-transaction.ts",
+  TokenCreate: "src/tests/token-service/test-token-create-transaction.ts",
+  TokenDelete: "src/tests/token-service/test-token-delete-transaction.ts",
   TokenDissociate:
     "src/tests/token-service/test-token-dissociate-transaction.ts",
   TokenFeeScheduleUpdate:
     "src/tests/token-service/test-token-fee-schedule-update-transaction.ts",
+  TokenFreeze: "src/tests/token-service/test-token-freeze-transaction.ts",
   TokenGrantKyc: "src/tests/token-service/test-token-grant-kyc-transaction.ts",
+  TokenMint: "src/tests/token-service/test-token-mint-transaction.ts",
+  TokenPause: "src/tests/token-service/test-token-pause-transaction.ts",
   TokenRevokeKyc:
     "src/tests/token-service/test-token-revoke-kyc-transaction.ts",
-  TokenPause: "src/tests/token-service/test-token-pause-transaction.ts",
-  TokenUnpause: "src/tests/token-service/test-token-unpause-transaction.ts",
-  TokenFreeze: "src/tests/token-service/test-token-freeze-transaction.ts",
   TokenUnfreeze: "src/tests/token-service/test-token-unfreeze-transaction.ts",
+  TokenUnpause: "src/tests/token-service/test-token-unpause-transaction.ts",
+  TokenUpdate: "src/tests/token-service/test-token-update-transaction.ts",
+  TokenWipe: "src/tests/token-service/test-token-wipe-transaction.ts",
   ALL: "src/tests/**/*.ts",
 };
