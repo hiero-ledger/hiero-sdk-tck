4d48710c3c2618b791c67452b713d83351856047
LukeBair <74842241+LukeBair@users.noreply.github.com>
2025-04-03 09:13:33 -0700

    codacy + network error handling
    (signed off)Revert "changing .env loading"
    
    This reverts commit 30fb4452236026bebd2bf260a005908bc3d0cced.
    
    Signed-off-by: LukeBair <74842241+LukeBair@users.noreply.github.com>

--- src/utils/helpers/network-config.ts
+++ src/utils/helpers/network-config.ts
@@ -1,4 +1,5 @@
 import dotenv from "dotenv";
+import * as process from "node:process";
 
 // Helper function to convert camelCase to SNAKE_CASE
 const toEnvFormat = (str: string): string => {
@@ -12,33 +13,34 @@ const toEnvFormat = (str: string): string => {
 export const getNetworkConfig = (
   network: string,
 ): Record<string, string | undefined> => {
-  dotenv.config();
-
   if (network === "testnet") {
-    if (
-      process.env.OPERATOR_ACCOUNT_ID === "***" ||
-      process.env.OPERATOR_ACCOUNT_PRIVATE_KEY === "***"
-    ) {
-      console.log(
-        "\n" +
-          "TESTNET_OPERATOR_ACCOUNT_ID and TESTNET_OPERATOR_ACCOUNT_PRIVATE_KEY must be set for testnet!",
-      );
+    dotenv.config({ path: ".env.testnet" });
+  } else if (network === "local") {
+    dotenv.config({ path: ".env.custom_node" });
+  } else {
+    console.log("Network config not found");
+    process.exit(1);
+  }
 
-      process.exit(1);
-    }
+  if (
+    process.env.OPERATOR_ACCOUNT_ID === "***" ||
+    process.env.OPERATOR_ACCOUNT_PRIVATE_KEY === "***"
+  ) {
+    console.log(
+      "\n" +
+        "TESTNET_OPERATOR_ACCOUNT_ID and TESTNET_OPERATOR_ACCOUNT_PRIVATE_KEY must be set for testnet!",
+    );
 
-    return {
-      nodeType: "testnet",
-      nodeTimeout: process.env.NODE_TIMEOUT,
-      mirrorNodeRestUrl: process.env.MIRROR_NODE_REST_URL,
-      operatorAccountId: process.env.OPERATOR_ACCOUNT_ID,
-      operatorAccountPrivateKey: process.env.OPERATOR_ACCOUNT_PRIVATE_KEY,
-      jsonRpcServerUrl: process.env.JSON_RPC_SERVER_URL,
-    };
+    process.exit(1);
+  }
+
+  if (!["testnet", "local"].includes(network)) {
+    console.log("invalid network config: ", network);
+    process.exit(1);
   }
 
   return {
-    nodeType: "local",
+    nodeType: network,
     nodeTimeout: process.env.NODE_TIMEOUT,
     nodeIp: process.env.NODE_IP,
     nodeAccountId: process.env.NODE_ACCOUNT_ID,
