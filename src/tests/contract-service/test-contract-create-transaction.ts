1b51ae05bc24bbf42a8dfda85d7c52b1a2ceb010
Ivan Ivanov <ivanivanov.ii726@gmail.com>
2025-08-11 16:13:54 +0300

    chore: fix
    Signed-off-by: Ivan Ivanov <ivanivanov.ii726@gmail.com>

--- src/tests/contract-service/test-contract-create-transaction.ts
+++ src/tests/contract-service/test-contract-create-transaction.ts
@@ -23,7 +23,7 @@ import {
 import { invalidKey } from "@constants/key-type";
 
 import { ErrorStatusCodes } from "@enums/error-status-codes";
-import { ContractFunctionParameters } from "@hashgraph/sdk";
+import { ContractFunctionParameters, ContractId } from "@hashgraph/sdk";
 
 const smartContractBytecode =
   "608060405234801561001057600080fd5b50336000806101000a81548173ffffffffffffffffffffffffffffffffffffffff021916908373ffffffffffffffffffffffffffffffffffffffff1602179055506101cb806100606000396000f3fe608060405260043610610046576000357c01000000000000000000000000000000000000000000000000000000009004806341c0e1b51461004b578063cfae321714610062575b600080fd5b34801561005757600080fd5b506100606100f2565b005b34801561006e57600080fd5b50610077610162565b6040518080602001828103825283818151815260200191508051906020019080838360005b838110156100b757808201518184015260208101905061009c565b50505050905090810190601f1680156100e45780820380516001836020036101000a031916815260200191505b509250505060405180910390f35b6000809054906101000a900473ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff163373ffffffffffffffffffffffffffffffffffffffff161415610160573373ffffffffffffffffffffffffffffffffffffffff16ff5b565b60606040805190810160405280600d81526020017f48656c6c6f2c20776f726c64210000000000000000000000000000000000000081525090509056fea165627a7a72305820ae96fb3af7cde9c0abfe365272441894ab717f816f07f41f07b1cbede54e256e0029";
@@ -2485,14 +2485,25 @@ describe.only("ContractCreateTransaction", function () {
       assert.fail("Should throw an error");
     });
 
-    it.skip("(#4) Create a contract with no adminKey", async function () {
+    it("(#4) Create a contract with no adminKey", async function () {
       const response = await JSONRPCRequest(this, "createContract", {
         ...commonContractParamsBase,
       });
       expect(response.status).to.equal("SUCCESS");
       expect(response.contractId).to.not.be.null;
 
-      await verifyContractCreationWithAdminKey(response.contractId, null);
+      const contractInfo = await consensusInfoClient.getContractInfo(
+        response.contractId,
+      );
+      const adminKeyContractID = ContractId.fromString(
+        response.contractId,
+      )._toProtobufKey().contractID?.contractNum;
+
+      expect(
+        contractInfo.adminKey
+          ?._toProtobufKey()
+          .contractID?.contractNum?.eq(adminKeyContractID as Long),
+      ).to.be.true;
     });
 
     it("(#5) Create a contract with an admin key too large (invalid)", async function () {
